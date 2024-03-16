b13d7cef32aa77073120984a1d1aebc7ac2ba9ee
raskal-dev <kalvinrasamyzo@gmail.com>
2024-03-16 08:14:05 +0300

    init

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+# une expression qui affiche Hello World
 print("Hello World")
+
+# une expression qui affiche Bonjour le monde
+print('Bonjour le monde')
+
+# une expression qui affiche 10 étoiles / produit un valeur de type str
+print('*' * 10)
+print('Bonjour ' * 5)
+
+# une expression qui affiche 6 / produit un valeur de type int
+print(2 * 3)
+
+# une expression qui affiche 6.0 / produit un valeur de type float
+print(2 * 3.0)
